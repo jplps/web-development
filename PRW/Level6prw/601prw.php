b0ae1ea4999e999a6ad44e6fda9c0fc288c137f1
@LPS <38426476+jplps@users.noreply.github.com>
2018-11-07 15:23:50 -0200

    Add files via upload

--- PRW/Level6prw/601prw.php
+++ PRW/Level6prw/601prw.php
@@ -6,12 +6,21 @@
         Diretório geral em debian-based distros: /var/www/html;
         Endereço http://localhost/path/to/file.php para acessar a aplicação;
 
+        Ex.: 1 - Script simples, utilizando construtor "echo" mostrando
+        mensagem principal e que contenha comandos php incorporados no html
+        (com comentários de uma ou mais linhas).
+
     -->
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>PHP Fundamentals</title>
+        <title>Ex.: 1</title>
+        <style>
+            body{text-align: center;}
+            h1{width: 60%; border-bottom: 2px solid #000; margin: auto; padding: 10px;}
+
+        </style>
         
     </head>
     <body>
