57476ba9df8deb7b179d8405a9627b5f87463c3c
JPLPS <joao.pedro.pacheco@hotmail.com>
2019-03-11 17:08:32 -0300

    Encountered lil bug but worked!

--- PRW/Level8prw/805prw.php
+++ PRW/Level8prw/805prw.php
@@ -5,10 +5,10 @@
     $students["1010-x3"] = ["Maria das Graças", 5, 9];
     $students["1010-x4"] = ["Calorine Prado", 4.2, 7.7];
 
-    //Referência:
-    echo "<pre>";
-    print_r($students);
-    echo "</pre>";
+    // //Referência:
+    // echo "<pre>";
+    // print_r($students);
+    // echo "</pre>";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -35,7 +35,7 @@
     <body>
         <h1>php arrays 5</h1>
 
-        <form action="" method="post">
+        <form id="form" action="" method="post">
             <fieldset>
                 <legend>Data Process</legend>
 
@@ -51,36 +51,119 @@
             </fieldset>
 
             <fieldset>
-                <legend>Other Operations</legend>
-                <button type="Approvals" form="" name="send">Approvals</button>
+                <legend>Operations</legend>
+                <button type="submit" form="form" name="approvals">Approvals</button>
 
-                <button type="Rendiment" form="" name="send">Rendiment</button>
+                <button type="submit" form="form" name="rendiment">Rendiment</button>
+
+                <button type="submit" form="form" name="distak">Distak</button>
             </fieldset>
         </form>
 
         <?php
-            
-            echo "
-            <table>
-                <tr>
-                    <th>Registration</th>
-                    <th>Name</th>
-                    <th>Av. Tests</th>
-                    <th>Av. Excs</th>
-                </tr>";
+            if(isset($_POST["rendiment"])){
+                //Recebe matrícula do select:
+                $reg = $_POST["reg"];
 
-                foreach ($students as $reg => $reg)
-                    echo "
+                //Acesso a dados:
+                $name = $students[$reg][0];
+                $avtests = $students[$reg][1];
+                $avexs = $students[$reg][2];
+
+                $avfinal = ($avtests * 7 + $avexs * 3)/10;
+
+                echo "<p>
+                    Selected Data:
+                    <br/><br/>
+                    Student: $name<br/>
+                    Registration: $reg<br/>
+                    Tests Average: $avtests<br/>
+                    Exercise Average: $avexs
+                    <br/><br/>
+                    Final Average: $avfinal
+                    <br/><br/>
+                ";
+            }
+
+            else if(isset($_POST["approvals"])){
+                //Auxilio para armazenamento de dados:
+                $auxArray = array();
+
+                foreach ($students as $reg => $interVector) {
+                    //Acessando dados do vetor interno de cada student:
+                    //Acesso a dados:
+                    $name = $students[$reg][0];
+                    $avtests = $students[$reg][1];
+                    $avexs = $students[$reg][2];
+
+                    $avfinal = ($avtests * 7 + $avexs * 3)/10;
+
+                    if ($avfinal >= 6) {
+                        //Guardar no array auxiliar:
+                        $auxArray[$reg][0] = $name;
+                        $auxArray[$reg][1] = $avfinal;
+                    }
+                }
+
+                // //Referência:
+                // echo "<pre>";
+                // print_r($auxArray);
+                // echo "</pre>";
+
+                echo "
+                <br/>
+                <table>
                     <tr>
-                        <td>$reg</td>";
-                        foreach ($reg as $test => $test)
-                            echo "<td>$test</td>";
-                    
-                    echo "</tr>";
-                
-                
+                        <th>Registration</th>
+                        <th>Name</th>
+                        <th>Final Av.</th>
+                    </tr>";
 
-            echo "</table>";
+                    foreach ($auxArray as $reg => $internVector)
+                        echo "
+                        <tr>
+                            <td>$reg</td>
+                            <td>$internVector[0] </td>
+                            <td>$internVector[1] </td>";
+                        
+                        echo "</tr>";
+                echo "</table>";
+            }
+
+            else if(isset($_POST["distak"])){
+                //Aux para armazenar matrícula e desempenho:
+                $auxArray = [];
+
+                foreach ($students as $reg => $internVector) {
+                    $avfinal = ($internVector[1] * 7 + $internVector[2] * 3)/10;
+
+                    //Guarda media final e matrícula no vetor auxiliar:
+                    $auxArray[$reg] = $avfinal;
+                }
+
+                // //Referência:
+                // echo "<pre>";
+                // print_r($auxArray);
+                // echo "</pre>";
+
+                //Função max() para percorrer vetor e checar maior elemento:
+                $highest = max($auxArray);
+
+                //Recuperar a matrícula no vetor auxiliar:
+                $regdistak = array_search($highest, $auxArray);
+
+                //Recuperando nome através da matricula:
+                $namedistak = $students[$regdistak][0];
+
+                echo "<p>
+                    Distak Data:
+                    <br/><br/>
+                    Student: $namedistak<br/>
+                    Registration: $regdistak
+                    <br/><br/>
+                    Final Average: $highest
+                </p>";
+            }
         ?>
     </body>
 </html>
