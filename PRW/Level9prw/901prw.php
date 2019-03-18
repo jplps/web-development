0533f6be02b66fbe4e8b7cf83dabec7922821a03
JPLPS <joao.pedro.pacheco@hotmail.com>
2019-03-18 16:47:58 -0300

    Functione

--- PRW/Level9prw/901prw.php
+++ PRW/Level9prw/901prw.php
@@ -16,10 +16,11 @@
 			body{width:66.6%; color:#666; margin: auto;}
 			h1{border-bottom:2px solid #666; margin:auto; padding:10px;
 			text-transform:capitalize; text-align:center; position:sticky; top:0;}
-			fieldset{margin-top:20px}
-			button, input {margin:5px 0;}
-			.align{display:inline-block;text-align:right;width:300px;}
-			
+			fieldset{margin-top:20px;}
+			button{display:block;margin:5px auto;}
+			input {margin:5px 0;}
+			.align{display:inline-block;text-align:right;width:50%;}
+
 		</style>
 	</head>
 
@@ -30,7 +31,7 @@
 			<fieldset>
 				<legend>Data Validation</legend>
 
-				<label for="">Input a Integer >= 0 :</label>
+				<label class="align">Input a Integer >= 0 :</label>
 				<input type="text" name="value" id="" />
 			</fieldset>
 
@@ -42,8 +43,33 @@
 		</form>
 
 		<?php
+			//Função para testar valor:
+			function valTest($x){
+				$test = filter_var($x, FILTER_VALIDATE_INT);
+
+				return $test;
+			}
+
 			if(isset($_POST["process"])){
-					
+				//Receber valor do input:
+				$val = $_POST["value"];
+
+				$return = valTest($val);
+
+				if($return){
+					$squareroot = sqrt($val);
+
+					$squarerooted = number_format($squareroot, 2, ".", ",");
+
+					echo "<p>
+						The square root of $val is ~$squarerooted!
+					</p>";
+				}
+
+				else
+					echo "<p>
+						This is not a nice value!
+					</p>";
 			}
 		?>
 	</body>
