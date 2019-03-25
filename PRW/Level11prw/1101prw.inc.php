e3fc8fcbf00ef41840f2c071fb1414706bb8b832
JPLPS <joao.pedro.pacheco@hotmail.com>
2019-03-25 17:04:21 -0300

    More sql with php

--- PRW/Level11prw/1101prw.inc.php
+++ PRW/Level11prw/1101prw.inc.php
@@ -1,5 +1,9 @@
 <?php
   class Student {
+    var $name;
+    var $registry;
+    var $g1, $g2, $g3;
 
+    
   }
 ?>
--- PRW/Level11prw/1101prw.php
+++ PRW/Level11prw/1101prw.php
@@ -16,24 +16,40 @@
 			body{width:66.6%; color:#666; margin: auto;}
 			h1{border-bottom:2px solid #666; margin:auto; padding:10px;
 			text-transform:capitalize; text-align:center; position:sticky; top:0;}
-			p{margin-top:40px;}
+			fieldset{margin-top:20px;}
+			input{margin:5px;}
+			p{float:right;}
+      table{margin: auto; border: 1px solid #666; border-collapse: collapse;}
+      caption{text-align: left; font-style: italic; margin-bottom: 5px;}
+      th{background: #999;}
+      td{padding-left: 10px;}
+      td.centralize, p{text-align: center;}
+      td, th{min-width:70px;border: 1px solid #b1b1b1;}
 		</style>
 	</head>
 
 	<body>
-		<h1>php + sql 1</h1>
+		<h1>php + sql</h1>
 
 		<form name="form" action="" method="post">
 			<fieldset>
-				<label for="">Student</label>
+				<label class="align">Student</label>
 				<input type="varchar" />
+				<br/>
 
-				<label for="">Register</label>
+				<label class="align">Registry</label>
 				<input type="number" />
+				<br/>
 
-				<label for="">Grade 1</label>
-				<input type="number" />
-			</fieldset>
+				<?php
+					for ($i=0; $i < 3; $i++) { 
+						echo "
+							<label class='align'>Grade $i</label>
+							<input type='number' name='g$i'/>
+							<br/>
+						";
+					}
+				?>
 		</form>
 
     <?php
