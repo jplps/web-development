37513dde9c45f9446e660df48b26b9ce271e2cf8
JPLPS <joao.pedro.pacheco@hotmail.com>
2019-04-09 14:00:11 -0300

    Fixed vars

--- PRW/Level11prw/1102prw/create-class.inc.php
+++ PRW/Level11prw/1102prw/create-class.inc.php
@@ -1,11 +1,11 @@
 <?php
 	class Project {
-    var title;
-    var coop;
-    var coor;
-    var datestamp;
-    var theme;
-    var methods;
+    var $title;
+    var $coop;
+    var $coor;
+    var $datestamp;
+    var $theme;
+    var $methods;
 
     function fetch($connection){
       $title = trim($connection->escape_string($_POST["title"]));
@@ -13,9 +13,10 @@
       $coor = trim($connection->escape_string($_POST["coor"]));
       $datestamp = trim($connection->escape_string($_POST["datestamp"]));
       $theme = trim($connection->escape_string($_POST["theme"]));
+
       //methods is an array: implode and explode process (SEC03)
       $aux0 = implode("-", $_POST["methods"]);  //transform array to string
-      $aux1 = trim($connection->escape_string($aux0); //clean up with trim
+      $aux1 = trim($connection->escape_string($aux0)); //clean up with trim
       $methods = explode("-", $aux1); //transform back to array
       
       //object constructor
@@ -28,8 +29,9 @@
     }
 
     function register($connection, $table){
-      //convert array to json
+      //convert php array to json
       $this->methods = json_encode($this->methods);
+      
       //in order with table fields!
       $sql = "INSERT $table VALUES (
         null,
@@ -38,7 +40,7 @@
         '$this->coor',
         '$this->datestamp',
         '$this->theme',
-        $this->methods
+        '$this->methods'
       )";
       $result = $connection->query($sql) || exit($connection->error);
       echo "<p>Success.</p>";
