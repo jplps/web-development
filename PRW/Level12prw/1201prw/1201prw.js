ba812ac65fa66f890e20d15754fdae7f7e91c21f
JPLPS <joao.pedro.pacheco@hotmail.com>
2019-05-20 16:35:35 -0300

    JavaScript initials

--- PRW/Level12prw/1201prw/1201prw.js
+++ PRW/Level12prw/1201prw/1201prw.js
@@ -1,2 +1,9 @@
 //Acesso a qualquer tag no navegador, em forma de objeto:
 var button = document.getElementById('btn') //Objeto *document* representa o DOM
+
+//Arrow Functions:
+onclick = () => {
+  button.style.color = 'white'
+  button.style.backgroundColor = 'red'
+  alert('style have changed')
+}
