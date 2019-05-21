2c92691f34182bea77a9df17e800e69ee373b652
JPLPS <joao.pedro.pacheco@hotmail.com>
2019-05-21 14:19:49 -0300

    JS Numeric Convertions

--- PRW/Level12prw/1203prw/1203prw.js
+++ PRW/Level12prw/1203prw/1203prw.js
@@ -1,23 +1,35 @@
-//Acesso a qualquer tag no navegador, em forma de objeto:
-var button = document.getElementById('btn') //Objeto *document* representa o DOM
-
-var firstparagraph = document.getElementById('first')
-var secondparagraph = document.getElementById('second')
-var thirdparagraph = document.getElementById('third')
-
-//Arrow Functions:
-const concatenate = () => {
-  var text1 = firstparagraph.innerHTML
-  var text2 = secondparagraph.innerHTML
-
-  var text3 = `Concatenated: <br />
-    ${text1} <br />
-    ${text2}
-  `
+function sum(){
+  //Todas as variáveis criadas dentro de uma função são locais;
+  var age = document.getElementById('age')
+  var balance = document.getElementById('balance')
   
-  thirdparagraph.innerHTML = text3
+  //Acessando conteúdo de uma caixa de formulário:
+  var num_1 = age.value
+  var num_2 = balance.value
+
+  /**
+   * ADIÇÃO E CONCATENAÇÃO
+   * 
+   * A prioridade do JavaScript ao encontrar o operador matemático 
+   * "+" é a concatenação. Para escapar deste padrão e explicitar a
+   * soma matemática, devemos converter as strings (tudo que vem de
+   * um formulário, para o js, é observado como string).
+   * 
+   * Obs.: NaN = Not a Number
+   */
+  num_1 = parseInt(num_1)
+  num_2 = parseInt(num_2)
+  
+  var sum = num_1 + num_2
+  alert("the sum is " + sum + "!")
+
+  //Todos os outros sinais são tratados normalmente!
+  var sub = num_1 - num_2
+  alert("the subtraction is " + sub + "!")
 }
 
+//Setting onclick event
+var button = document.getElementById('btn') //Objeto *document* representa o DOM
 button.onclick = () => {
-  concatenate()
+  sum()
 }
