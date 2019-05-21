70c50b6054db24987267ed9c170b31a7c4b7e06b
JPLPS <joao.pedro.pacheco@hotmail.com>
2019-05-21 15:03:52 -0300

    JS Entry Boxes

--- PRW/Level12prw/1204prw/1204prw.js
+++ PRW/Level12prw/1204prw/1204prw.js
@@ -1,1 +1,6 @@
-//Inicializando prompt
+//Inicializando prompt - pedindo nome completo (null se não for providenciado)
+var name = prompt('Name:', 'John Doe')
+
+//Mudando dinamicamente o conteúdo da página conforme teste lógico:
+if(name)
+  document.getElementById('paragraph').innerHTML = 'Hi there ' + name + '!'
