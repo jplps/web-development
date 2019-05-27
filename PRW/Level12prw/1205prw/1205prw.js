28faf028030d66c4865c4de090f09a6ddf122efd
JPLPS <joao.pedro.pacheco@hotmail.com>
2019-05-27 14:28:08 -0300

    More js

--- PRW/Level12prw/1205prw/1205prw.js
+++ PRW/Level12prw/1205prw/1205prw.js
@@ -1,3 +1,15 @@
 function putBorder(paragraph){
   paragraph.style.boxShadow = "0 0 2px 2px rgba(0,0,0,0.5)"
 }
+
+function changeText(button){
+  var value = button.value
+  if(value == "1"){
+    button.innerText = "i have changed see"
+    button.value = "2"
+  }
+  else{
+    button.innerText = "change my text brow"
+    button.value = "1"
+  }
+}
