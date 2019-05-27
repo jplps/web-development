e658b62caa7726689332e8ccc9b80444f3a6572b
JPLPS <joao.pedro.pacheco@hotmail.com>
2019-05-27 15:43:24 -0300

    JS funcs

--- PRW/Level12prw/1207prw/1207prw.js
+++ PRW/Level12prw/1207prw/1207prw.js
@@ -1,5 +1,7 @@
-function sumMe(params) {
-  
+//Definindo função ordinária
+function sumMe(n1, n2) {
+  //Expressões matemáticas devem ser isoladas entre (), ou o sinal de + concatenará variáveis
+  return alert("sum = " + (n1 + n2))
 }
 
 //Invocar função
