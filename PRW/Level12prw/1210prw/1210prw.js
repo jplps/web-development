f2be8d437576c917f44053b86bc5ef9bbbfaf519
JPLPS <joao.pedro.pacheco@hotmail.com>
2019-05-28 14:13:33 -0300

    More js

--- PRW/Level12prw/1210prw/1210prw.js
+++ PRW/Level12prw/1210prw/1210prw.js
@@ -5,14 +5,16 @@ var paragraph = document.getElementById('container')
 
 //Declaração de literal
 var extract = () => {
-  //Acessando o valor numérico
+  //Acesso ao valor numérico
   var num = input.value
   //Extração da raíz com a lib Math
   var square_root = Math.sqrt(num)
   //Formatação local com arredondamento
   // square_root = square_root.toLocaleString('pt-br', {maximumFractionDigits:2})
-  //Alterando conteúdo do container
+  //Alteração do conteúdo do container
   paragraph.innerText = square_root
+  //Ativação da classe do container (sem influenciar diretamente o css com paragraph.style.display)
+  paragraph.setAttribute('class', 'show')
 }
 
 //Associação de cada evento ao seu objeto com literais
