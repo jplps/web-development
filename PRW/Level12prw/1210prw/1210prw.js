dc2c97b1beab321cfc67f60d06931432d5cba517
JPLPS <joao.pedro.pacheco@hotmail.com>
2019-05-28 13:48:25 -0300

    More js

--- PRW/Level12prw/1210prw/1210prw.js
+++ PRW/Level12prw/1210prw/1210prw.js
@@ -1,18 +1,19 @@
-//Criando objetos para manipulações
-var button = document.getElementById("btn")
-var input = document.getElementById("num")
-var paragraph = document.getElementById("container")
+//Criação objetos para manipulações
+var button = document.getElementById('btn')
+var input = document.getElementById('num')
+var paragraph = document.getElementById('container')
 
-//Declarando literal
+//Declaração de literal
 var extract = () => {
   //Acessando o valor numérico
   var num = input.value
   //Extração da raíz com a lib Math
   var square_root = Math.sqrt(num)
-  // square_root = square_root.toLocaleString('pt-br') //Formatação local
+  //Formatação local com arredondamento
+  // square_root = square_root.toLocaleString('pt-br', {maximumFractionDigits:2})
   //Alterando conteúdo do container
   paragraph.innerText = square_root
 }
 
-//Associar cada evento ao seu objeto com literais
+//Associação de cada evento ao seu objeto com literais
 button.addEventListener('click', extract)
