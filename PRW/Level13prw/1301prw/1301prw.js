984e1b5c406d426c308fda3b47c579f48ed2cb31
JPLPS <joao.pedro.pacheco@hotmail.com>
2019-06-03 15:10:07 -0300

    JS Form validation

--- PRW/Level13prw/1301prw/1301prw.js
+++ PRW/Level13prw/1301prw/1301prw.js
@@ -1,8 +1,22 @@
 //Acesso ao formulário, atribuição de objeto
 var form = document.getElementById('form')
+/**
+ * Literal de função para validar formulário, recebendo evento e 
+ * 
+ * Este evento é um objeto oferecido pelo js sempre que criarmos 
+ * uma função que trata de um evento. Pode receber qualquer nome
+ * para referência dentro do escopo.
+*/
+const validateForm = e => {
+  //Cancelar evento padrão (navegador não dispara o evento e)
+  e.preventDefault()
+  //Criação de objetos para mostrar erros
+  var mailerror = document.getElementById('mailerror')
+  var passerror = document.getElementById('passerror')
+  var passconfirmerror = document.getElementById('passconfirmerror')
+  var registryerror = document.getElementById('registryerror')
+  var justificationerror = document.getElementById('justificationerror')
 
-//Literal de função para validar formulário
-const validateForm = () => {
   
 }
 
