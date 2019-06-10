41139b3ddbdd8a62589fbbc5d60450e6924e1547
JPLPS <joao.pedro.pacheco@hotmail.com>
2019-06-10 15:32:21 -0300

    AJAXfundamentals

---
+++ PRW/Level14prw/1401prw/1401prw.js
@@ -0,0 +1,27 @@
+//Criando objeto da div de retorno
+var div_return = document.getElementById('return')
+
+const processAjax = event => {
+  //1. Criar objeto ajax
+  var ajax = new XMLHttpRequest()
+
+  //2. Tratamento de resposta vinda do servidor
+  ajax.onload = () => {
+    if(ajax.status == 200){
+      //Traz o comando echo do arquivo 1401prw.php
+      div_return.innerHTML = ajax.responseText
+      div_return.setAttribute('class', 'showme')
+    }
+  }
+
+  //3. Iniciar conexão entre cliente e servidor com método open()
+  ajax.open('GET', '1401prw.php', true)
+
+  //4. Enviar requisição ao servidor
+  ajax.send(null)
+
+  event.preventDefault()
+}
+
+//Criando objeto link e atribuindo evento
+var link = document.getElementById('link').addEventListener('click', processAjax)
---
+++ PRW/Level14prw/1401prw/1401prw.php
@@ -0,0 +1,8 @@
+<?php
+  echo "<p>
+    this block was brougth by PHP (server), by AJAX, and inserted
+    dinamically in the current page, without creating or uploading
+    a new one. the content sent by PHP and recieved by AJAX can
+    contain pure text, HTML, CSS, and JavaScript code.
+  </p>";
+?>
