47a75d92e7207dfbfce3931e75688cf78db06b46
JPLPS <joao.pedro.pacheco@hotmail.com>
2019-06-11 14:30:44 -0300

    Finished AJAX with post method

---
+++ PRW/Level14prw/1403prw/1403prw.js
@@ -0,0 +1,43 @@
+const processAjax = e => {
+  var ajax = new XMLHttpRequest()
+  
+  //Acessar todos os elementos que irão transferir dados pro PHP
+  var name = myform.name.value
+  var weight = myform.weight.value
+  var height = myform.height.value
+  
+  //Criando objeto da div de retorno
+  var p_return = document.getElementById('return')
+
+  //POST requer abordagem sequencial:
+  //1. Abrir requisição SEM ALTERAÇÃO DA URL
+  ajax.open('POST', '1403prw.php', true)
+  
+  /**
+   * 2. Envío ao PHP do cabeçalho http por meio do objeto AJAX.
+   * AVISO: Este comando deve estar APÓS o método open()! 
+   */
+  ajax.setRequestHeader('content-type', 'application/x-www-form-urlencoded')
+
+  //3. A montagem da query string deve vir antes do método send()
+  var data = "name=" + name + "&weight=" + weight + "&height=" + height
+
+  //4. Enviar dados via ajax no corpo do protocolo HTTP
+  ajax.send(data)
+
+  //5. Implementando resposta do JS no retorno do AJAX do servidor
+  ajax.onload = () => {
+    if(ajax.status == 200){
+      p_return.innerHTML = ajax.responseText
+      p_return.setAttribute('class', 'showme')
+    }
+  }
+
+  e.preventDefault()
+}
+
+/**
+ * JavaScript automaticamente disponibiliza um objeto formulario
+ * quando utilizamos o atributo name da tag form 
+ */
+myform.addEventListener('submit', processAjax)
---
+++ PRW/Level14prw/1403prw/1403prw.php
@@ -0,0 +1,24 @@
+<?php
+  //Recebendo dados via PHP
+  $name = $_POST["name"];
+  $weight = $_POST["weight"];
+  $height = $_POST["height"];
+
+  //Calcular o IMC
+  $imc = $weight / pow($height, 2);
+
+  //Definir situação
+  if($imc < 17) $situation = "Extreme Under Weight";
+  elseif($imc < 18.5) $situation = "Under Weight";
+  elseif($imc < 25) $situation = "Regular Weight";
+  elseif($imc < 30) $situation = "Above Weight";
+  elseif($imc < 35) $situation = "Obesity I";
+  elseif($imc < 40) $situation = "Obesity II";
+  else $situation = "Obesity III";
+
+  //Formatar número
+  $imc = number_format($imc, 1, ",", ".");
+  
+  //Tag <em> destaca com ênfase seu conteúdo em itálico
+  echo "User <em>$name</em>:\n\tIMC: <em>$imc</em>\n\tSituation: <em>$situation</em>";
+?>
